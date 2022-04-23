5b07cddec1665eeaea66e4b1b7e324ecd6fafd13
unknown <nav01@mail.fresnostate.edu>
2022-04-22 22:10:08 -0700

    Implemented asset tag search forwarding.

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import os
+import requests
 
 app = Flask(__name__)
+SNIPE_IT_KEY = os.environ.get('SNIPE_IT_KEY')
+SNIPE_IT_BASE_API_URL = os.environ.get('SNIPE_IT_BASE_API_URL')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+SNIPE_IT_REQUEST_HEADERS = {
+    'Authorization': ('Bearer ' + SNIPE_IT_KEY),
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+
+@app.route("/hardware/bytag/<asset_tag>")
+def get_asset_by_tag(asset_tag):
+    return requests.get(SNIPE_IT_BASE_API_URL + f'/hardware/bytag/{asset_tag}', headers=SNIPE_IT_REQUEST_HEADERS).json()
